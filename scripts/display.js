7dafbe76ab1de75baae7420632d1a431fe3ba75f
Alexis Peñuñuri <alxpesa@gmail.com>
2024-03-13 20:55:06 -0700

    Reverted changes

--- scripts/display.js
+++ scripts/display.js
@@ -125,7 +125,7 @@ function displayCards() {
         <p><span class=bold>Service: </span>${pet.service}</p>
         <p><span class=bold>Breed: </span>${pet.breed}</p>
         <p><span class=bold>Payment method: </span>${pet.method}</p>
-        <p><button class="btn-small" onclick="deletePet(${i})">Delete</button></p>
+        <p><button class="btn-small" onclick="deletePet(${i})"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M6 7H5v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7H6zm4 12H8v-9h2v9zm6 0h-2v-9h2v9zm.618-15L15 2H9L7.382 4H3v2h18V4z"></path></svg></button></p>
       </div>
     `;
   }
